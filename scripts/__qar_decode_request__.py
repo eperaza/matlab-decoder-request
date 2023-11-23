da74717fa06205ad7e98896ef8a4bbe0a15e9ea1
Eduardo Peraza Acosta <eduardo.perazaacosta@boeing.com>
2023-11-23 08:35:33 -0800

    fix unzipping

--- scripts/__qar_decode_request__.py
+++ scripts/__qar_decode_request__.py
@@ -462,10 +462,11 @@ class _DecodeRequest:
                             zipinfo.filename
                         ).lower().__contains__(".raw"):
                             print("Unzipping: ", zipinfo.filename, flush=True)
+                            #shortuuid=str(uuid.uuid4())[:5]
                             if (zipinfo.filename).lower().__contains__(".dat"):
-                                zipinfo.filename = "raw_" + str(uuid.uuid4()) + ".dat"
+                                zipinfo.filename = f"{item.name}.dat" 
                             if (zipinfo.filename).lower().__contains__(".raw"):
-                                zipinfo.filename = "raw_" + str(uuid.uuid4()) + ".raw"
+                                zipinfo.filename = f"{item.name}.raw" 
                             zipdata.extract(zipinfo, qar_dir_in)
                             print("File unzipped", flush=True)
                             zipdata.close()
